ffadf875b9245dcddcfbfb16699466c61dc6b388
Akash Mishra <akash.mishra13@gmail.com>
2023-04-25 03:53:14 -0400

    fix

--- data_collector/main.py
+++ data_collector/main.py
@@ -1,9 +1,12 @@
 from financial_apis import get_eligible_entities
 from models import Entity
 from tqdm import tqdm
+import os
 
 def collect_data_from_financialprep():
+    ignore_entities = [os.path.splitext(filename)[0] for filename in os.listdir('./data/raw_files/')]
     entities = get_eligible_entities()
+    entities = list(set(entities)-set(ignore_entities))
     for i in tqdm(range(len(entities))):
         symbol = entities[i]
         entity = Entity(symbol)
@@ -14,3 +17,7 @@ def collect_data_from_financialprep():
 
 if __name__=="__main__":
     collect_data_from_financialprep()
+
+
+    
+    
